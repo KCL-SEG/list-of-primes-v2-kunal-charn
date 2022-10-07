c0a26997dad2d32dafa09f4fefa595536b0f55e0
Kunal Charn <charnkunal@gmail.com>
2022-10-07 15:12:06 +0100

    done

--- primes.py
+++ primes.py
@@ -1,6 +1,14 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+import math
 
 def primes(number_of_primes):
     list = []
+    if number_of_primes <= 0:
+        raise ValueError()
+    num = 2
+    while(len(list) != number_of_primes):
+        if all(num%i!=0 for i in range(2,int(math.sqrt(num))+1)):
+            list.append(num)
+        num = num+1
     return list
